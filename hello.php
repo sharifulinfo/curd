164776ee677204de78e9be6dc012982e30da8239
Shariful Islam <shariul.info55@gmail.com>
2018-04-14 16:14:19 +0600

    Add git class here..

---
+++ hello.php
@@ -0,0 +1,18 @@
+<?php
+	/**
+	* GitHub Class
+	*/
+	class GitHub{ 
+		// function __construct($data){ 
+			
+		// }
+		public $a=50;
+		public $b=60;
+		public $sum;
+
+		public function($this->a,$this->b){
+			$this->sum = $this->a+$this->b;
+			return $this->sum;
+		}
+	}
+
